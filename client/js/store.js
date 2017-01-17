b2bc18ae15da3bd29cd41a093c05fab90b9a3257
Hannah <hannahjmccain@gmail.com>
2017-01-17 15:18:02 -0500

    Added redux store.

--- client/js/store.js
+++ client/js/store.js
@@ -1,1 +1,10 @@
 import thunk from 'redux-thunk';
+import reducer from './reducers/reducers';
+import createLogger from 'redux-logger';
+
+const logger = createLogger();
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export default createStore(reducer, composeEnhancers(
+	applyMiddleware(thunk, logger)
+));
